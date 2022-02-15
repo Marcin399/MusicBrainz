c8f3bd26c979a70b1b37b3276c9cf2012b856abf
Marcin Janiczek <inspiremarcin@gmail.com>
2022-02-15 19:33:09 +0100

    Initial Commit

---
+++ MusicBrainz/MusicBrainzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBrainzApp.swift
+//  MusicBrainz
+//
+//  Created by Marcin on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicBrainzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
